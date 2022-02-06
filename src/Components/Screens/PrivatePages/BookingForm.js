a907359d1d3209632f722f0a5f44bf21f5bbd2a0
surendran <surendransakthivel6@gmail.com>
2022-02-06 23:47:29 +0530

    updated booking.js

--- src/Components/Screens/PrivatePages/BookingForm.js
+++ src/Components/Screens/PrivatePages/BookingForm.js
@@ -11,6 +11,7 @@ const BookingForm = () => {
   const { userId, setLoggedIn, getLoggedInState } = useContext(userContext);
   const [seat, setSeat] = useState(undefined);
   const [berth, setBerth] = useState(undefined);
+  const [ladychild, setLadychild] = useState(false);
 
   const history = useHistory();
   useEffect(() => {
@@ -20,9 +21,9 @@ const BookingForm = () => {
     let token = localStorage.getItem("authToken");
     const bookingDetails = values;
     bookingDetails["userId"] = userId;
-    bookingDetails["seat"] = seat.seatNo;
+    bookingDetails["seat"] = bookingDetails.age < 5 ? null: seat.seatNo;
+    const berthAvailble = berth.find((x) => x.name === bookingDetails.berth);
     //const [booking, setbooking] = useState(undefined);
-
     try {
       if (token) {
         const result = await axios.post(
@@ -46,7 +47,11 @@ const BookingForm = () => {
             progress: undefined,
           });
           history.push("/views");
-          deleteSeat();
+          if(bookingDetails.age > 5 ){
+            deleteSeat();
+            berthAvailble.available = Number(berthAvailble?.available) - 1;
+            await updateBerth(berthAvailble);
+          }
         }
         console.log(result.data.ticket);
         // setbooking(result.data.ticket);
@@ -100,6 +105,7 @@ const BookingForm = () => {
     getBerthDetails();
     getLoggedInState();
   };
+
   const getSeat = async (values) => {
     let token = localStorage.getItem("authToken");
 
@@ -219,6 +225,68 @@ const BookingForm = () => {
     }
   };
 
+  const updateBerth = async (berthAvailble) => {
+    let token = localStorage.getItem("authToken");
+
+    try {
+      if (token) {
+        const result = await axios.put(
+          `http://localhost:5000/api/private/UpdateBerth/${berthAvailble._id}`,
+          { berthAvailble: berthAvailble },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+      } else {
+        toast.error("Something wen't wrong", {
+          position: "top-right",
+          autoClose: false,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setTimeout(() => {
+          localStorage.removeItem("authToken");
+          setLoggedIn(false);
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error.response);
+      if (error.response.data.message) {
+        toast.error("Invalid URL", {
+          position: "top-right",
+          autoClose: false,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        toast.error("Something wen't wrong", {
+          position: "top-right",
+          autoClose: false,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setTimeout(() => {
+          localStorage.removeItem("authToken");
+          setLoggedIn(false);
+        }, 2000);
+      }
+    }
+  };
+
+
   const getBerthDetails = async (values) => {
     let token = localStorage.getItem("authToken");
 
@@ -280,44 +348,66 @@ const BookingForm = () => {
     }
   };
 
-  const checkBerthAvail = (berthDetails)=>{
-   const rac = berthDetails.find((x) => x.name === "RAC");
-        const waiting = berthDetails.find((x) => x.name === "WaitingList");
-        let count = 0;
-        berthDetails.forEach((data) => {
-          count = count + Number(data.available);
-        });
-        count = count - 28;// rac total + waiting total
-        if(count<=63){
-         berthDetails = berthDetails.filter(
-          (data)=>{
-              return Number(data.available) !== 0 && data.name !== "RAC" && data.name !=="WaitingList";
-          }           
-          );
-        } else if (rac.available!==0 ){
-         berthDetails = berthDetails.filter(
-            (data) => {
-              return Number(data.available) !== 0  && data.name !== "WaitingList";
-            }
-          );          
-        }else{
-         berthDetails = berthDetails.filter(
-            (data) => {
-              return Number(data.available) !== 0;
-            }
-          );     
+  const checkBerthAvail = (berthDetails) => {
+    const rac = berthDetails.find((x) => x.name === "RAC");
+    const waiting = berthDetails.find((x) => x.name === "WaitingList");
+    let count = 0;
+    berthDetails.forEach((data) => {
+      count = count + Number(data.available);
+    });
+    count = count - 28;// rac total + waiting total
+    if (count <= 63) {
+      berthDetails = berthDetails.filter(
+        (data) => {
+          return Number(data.available) !== 0 && data.name !== "RAC" && data.name !== "WaitingList";
         }
-        setBerth(berthDetails);
+      );
+    } else if (rac.available !== 0) {
+      berthDetails = berthDetails.filter(
+        (data) => {
+          return Number(data.available) !== 0 && data.name !== "WaitingList";
+        }
+      );
+    } else {
+      berthDetails = berthDetails.filter(
+        (data) => {
+          return Number(data.available) !== 0;
+        }
+      );
+    }
+    setBerth(berthDetails);
 
   }
 
-// const handleAge =(data,val)=> {
-// console.log(data)
-//   }
+  // const handleAge =(data,val)=> {
+  // console.log(data)
+  //   }
 
   return (
     <div className="container align">
-      <h1 className="heading">Book ticket</h1>
+      <div className="heading">
+        <h1 >Book ticket</h1>
+        </div>
+
+      <label class="switch">
+        <input type="checkbox"
+          onChange={() => {
+            if (!ladychild) {
+              const details = berth.filter(
+                (data) => {
+                  return Number(data.available) !== 0 && data.name === "LowerBerth";
+                }
+              );
+              setBerth(details);
+              setLadychild(true);
+            } else {
+              setLadychild(false);
+              getBerthDetails();
+            }
+          }
+          } /> Lady with  child below 5 year old
+        <span class="slider round"></span>
+      </label>
       <Formik
         initialValues={{
           bookingName: "",
@@ -353,6 +443,7 @@ const BookingForm = () => {
               name="gender"
               id="gender"
             >
+              <option hidden>-- select gender --</option>
               <option>Male</option>
               <option>Female</option>
               <option>Other</option>
@@ -367,18 +458,33 @@ const BookingForm = () => {
               id="age"
               name="age"
               placeholder="Age"
+              onBlur={e => {
+                let ageVal = e.currentTarget.value;
+                if (ageVal > 60 || ageVal < 5) {
+                  const details = berth.filter(
+                    (data) => {
+                      return Number(data.available) !== 0 && data.name === "LowerBerth";
+                    }
+                  );
+                  setBerth(details);
+                } else {
+                  getBerthDetails();
+                }
+              }
+              }
             />
-
             <ErrorMessage name="age" className="text-danger" />
           </div>
 
           <div className="form-group  col-sm-12">
             <Field as="select" className="form-control" name="berth" id="berth">
+              <option hidden>-- select berth preference --</option>
               {berth?.map((data) => {
                 return <option>{data.name}</option>;
               })}
             </Field>
             <ErrorMessage name="berth" className="text-danger" />
+
           </div>
           <div className="button-container">
             <button type="submit" className="btn btn-primary">
